8574cd27969add25759186869bbf05ba74cd045f
Artem Ignatov <ghettoarabdog@gmail.com>
2022-06-04 17:15:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_M_3._10._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            Console.WriteLine("Здравствуйте! \nСколько у Вас на руках карт?");
+            int n = int.Parse(Console.ReadLine());
+            for (int i=1; i<=n; i++)
+                {
+                Console.WriteLine($"Введите вашу {i}-ю карту:");
+                string card = Console.ReadLine();
+                    switch (card)
+                    {
+                        case "J":
+                            sum += 10;
+                            break;
+                        case "Q":
+                            sum += 10;
+                            break;
+                        case "K":
+                            sum += 10;
+                            break;
+                        case "T":
+                            sum += 10;
+                            break;
+                        default:
+                            sum += int.Parse(card);
+                            break;
+                    }
+                }
+            Console.WriteLine($"Сумма Ваших карт равна: {sum}");
+            Console.ReadKey();
+        }
+    }
+}
